18e8451b2c170da856e176532d4488f2dfa8b86f
Whitney Reid <whitreid@hotmail.com>
2019-08-20 15:44:29 -0400

    Adding files 🔥

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//wait for the document to be ready
+//create an array of questions in an array with an object
+//when the form submits (add an event listener/when enter is pressed)
+//find the users choices (multiple choice)
+//save the users choice as variables
+//select appropriate array from questions object based on user choice
+// print title of cafe onto the page
+
+/* Visuals
+ I find images of celebrities and what describes the question
+ 
+ */
