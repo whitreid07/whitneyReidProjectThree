b0040a2bcc0afdd2c67c4a555ccb200cc3f217ad
Whitney Reid <whitreid@hotmail.com>
2019-08-23 11:07:40 -0400

    js version 1

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@ let personalityQuiz = {
       answers: [
         {
           answer: "Im powerful and silly",
-          result: "Cardi B"
+          result: "cardiB"
         },
         {
           answer: "Arrogant, but charming",
-          result: "Kanye"
+          result: "kanye"
         },
         {
           answer: "Ambitious and fierce",
-          result: "Beyonce"
+          result: "beyonce"
         },
       ]
     },
@@ -25,11 +25,11 @@ let personalityQuiz = {
       answers: [
         {
           answer: "red",
-          result: "Cardi B"
+          result: "cardiB"
         },
         {
           answer: "purple",
-          result: "Beyonce"
+          result: "beyonce"
         },
         {
           answer: "blue",
@@ -42,24 +42,24 @@ let personalityQuiz = {
 
 // questions, results, and personality descriptions
 //four celebrities
-results: ["CardiB", "Beyonce", "Kanye"],
+// results: ["CardiB", "Beyonce", "Kanye"],
 
-  descriptions = [
-    {
+descriptions = [
+  {
 
-      //Cardi B
-      celebrity: "Cardi B \n You're a powerful, confident badass who turns heads any time you walk in a room. You have a natural charm that people gravitate towards — once you've hooked them in, they don't want to escape!"
-    },
-    {
-      //Beyonce 
-      celebrity: "Beyonce \n You're a fierce, ambitious person with a kind heart. You never give up on your friends, and you always stick by the ones you love."
-    },
+    //Cardi B
+    celebrity: "Cardi B \n You're a powerful, confident badass who turns heads any time you walk in a room. You have a natural charm that people gravitate towards — once you've hooked them in, they don't want to escape!"
+  },
+  {
+    //Beyonce 
+    celebrity: "Beyonce \n You're a fierce, ambitious person with a kind heart. You never give up on your friends, and you always stick by the ones you love."
+  },
 
-    {
-      //Kanye West 
-      celebrity: "Kanye West \n Extremely intelligent and very smart, there's a whole lot more to you then some people realize. Being different doesn't scare you. You love to be the center of attention."
-    }
-  ]
+  {
+    //Kanye West 
+    celebrity: "Kanye West \n Extremely intelligent and very smart, there's a whole lot more to you then some people realize. Being different doesn't scare you. You love to be the center of attention."
+  }
+]
 
 
 
@@ -67,37 +67,32 @@ results: ["CardiB", "Beyonce", "Kanye"],
 //wait for the document to be ready
 $(document).ready(function () {
 
-
-
-  //check the user's choices array for the most frequent string
-
-
-  //find the highest occurence in responses using for loop; use conditions
-
-  // const filteredArray = [];
-  // for (let i = 0; i < drinkArray.length; i++) {
-  //   // console.log(userPrice);
-  //   // console.log(drinkArray[i].price);
-  //   if (userPrice === drinkArray[i].price) {
-  //     //push selected cafes into filtered array
-  //     filteredArray.push(drinkArray[i].title);
-
-  //   }
-  // }
-
+  //when the form submits (add an event listener/when enter is pressed)
   $('form').on('submit', function (event) {
     event.preventDefault();
-    console.log('submit')
-    //find the users choices (multiple choice) as variables using jquery's .val()
-    //find the users choices
+    // console.log('submit')
+
+    //find the users choice (multiple choice) as variables using jquery's .val()
     //save the users choice as variables
+    const userPersonalityType = $('input[name="personalityType"]:checked').val();
+    const userColourType = $('input[name ="colourType"]:checked').val();
+    // console.log(userColourType);
+
+
+    //select appropriate array from questions object based on user choice
+
+    const celebrityArray = personalityQuiz[userPersonalityType];
+    console.log(celebrityArray);
+
+
     const userInput = [];
 
     let cardiB = 0
     let beyonce = 0
     let kanye = 0
 
-    //for each item in the array find out if it matches the user's choice
+
+    //check the user's choices array for the most frequent string
     const userChoice = [];
     for (i = 0; i < userChoice.length; i++) {
       if (userChoice[i] === 'cardiB') {
@@ -111,7 +106,6 @@ $(document).ready(function () {
       }
     }
 
-    // select appropriate array from drinks object based on user choice
 
     if (cardiB > beyonce && cardi > kanye) {
       //do something!
@@ -129,15 +123,14 @@ $(document).ready(function () {
     }
 
 
-    // const userChoice = personalityQuiz[userChoice];
-    userInput.push('input:checked').val();
-    console.log(userChoice);
+    //for each item in the array find out if it matches the user's choice
+
 
   });
 
 });
 //use frequent string from the array, search the people array to find matching celebrity
-//when the form submits (add an event listener/when enter is pressed)
+
 
 
   // print title of celebrity image onto the page
