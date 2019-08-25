15da8497f7e36ddcf8217b95e1a44a52a7f7e726
Whitney Reid <whitreid@hotmail.com>
2019-08-25 13:23:09 -0400

    looking good added transiions 😎

--- js/script.js
+++ js/script.js
@@ -65,22 +65,44 @@ const descriptions = [
     celebrity: "Kanye West",
     celebrityDescription: "You like to be the center of attention, but that's only because you see the world in a unique way and want to share it with others.",
     image: "https://media.giphy.com/media/9qvkhbHg3NOKI/giphy.gif"
+  },
+  {
+    //Wild Card
+    celebrity: "Wild Card",
+    celebrityDescription: " Oh, looks like you got the wild choice.",
+    image: "https://media.giphy.com/media/l3q2B4Mlsw4HFpVE4/giphy.gif"
+
   }
+
 ]
 
-const descriptionsObject = {
-  // cardiB: "Cardi B \n You're a powerful, confident badass who turns heads any time you walk in a room. You have a natural charm that people gravitate towards — once you've hooked them in, they don't want to escape!",
-  // beyonce: "Beyonce \n You're a fierce, ambitious person with a kind heart. You never give up on your friends, and you always stick by the ones you love.",
-  // kanye: "Kanye West \n Extremely intelligent and very smart, there's a whole lot more to you then some people realize. Being different doesn't scare you. You love to be the center of attention.",
-}
+
 
 //wait for the document to be ready
 $(document).ready(function () {
+
+  //When button is click move 100vh to next question
+  $('.headerButton').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $(".sectionOneContainer").offset().top
+    }, 1000);
+  });
+  $('.sectionOneButton').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $(".sectionTwoContainer").offset().top
+    }, 1000);
+  });
+  $('.submitContainer').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $(".sectionThreeContainer").offset().top
+    }, 1000);
+  });
 
   //when the form submits (add an event listener/when enter is pressed)
   $('form').on('submit', function (event) {
     event.preventDefault();
     // console.log('submit')
+
 
     //find the users choice (multiple choice) as variables using jquery's .val()
     //save the users choice as variables
@@ -97,7 +119,6 @@ $(document).ready(function () {
 
     // const celebrityArray = personalityQuiz[userPersonalityType];
     // console.log(celebrityArray);
-
 
     //check the user's choices array for the most frequent string
     //find the highest occurence in responses using for loop; use conditions
@@ -125,7 +146,7 @@ $(document).ready(function () {
       }
     }
 
-    // console.log(cardiB, beyonce, kanye);
+    console.log(cardiB, beyonce, kanye);
 
     //use frequent string from the array, search the people array to find matching celebrity
 
@@ -145,11 +166,16 @@ $(document).ready(function () {
                                         <p>${descriptions[1].celebrityDescription}</p>
                                         <img src=${descriptions[1].image} />`);
     }
-    else {
-      console.log(descriptionsObject.default);
+    else if (kanye > cardiB && kanye > beyonce) {
       $('.sectionThreeContainer').html(`<h2> ${descriptions[2].celebrity}</h2>
                                         <p>${descriptions[2].celebrityDescription}</p>
                                         <img src=${descriptions[2].image} />`);
+    }
+    else {
+      // console.log(descriptionsObject.default);
+      $('.sectionThreeContainer').html(`<h2> ${descriptions[3].celebrity}</h2>
+                                        <p>${descriptions[3].celebrityDescription}</p>
+                                        <img src=${descriptions[3].image} />`);
     }
 
   });
