c90563d07400f315dddee08be623b42cf1f68f32
Whitney Reid <whitreid@hotmail.com>
2019-08-25 03:13:55 -0400

    styling 💥👍

--- js/script.js
+++ js/script.js
@@ -49,24 +49,29 @@ const descriptions = [
   {
 
     //Cardi B
-    celebrity: "Cardi B \n You're a powerful, confident badass who turns heads any time you walk in a room. You have a natural charm that people gravitate towards — once you've hooked them in, they don't want to escape!",
+    celebrity: "Cardi B",
+    celebrityDescription: "You're a powerful, confident badass who turns heads any time you walk in a room. You have a natural charm that people gravitate towards — once you've hooked them in, they don't want to escape!",
     image: "https://media.giphy.com/media/3o751Yxe9UjX26BZbG/giphy.gif"
   },
   {
     //Beyonce 
-    celebrity: "Beyonce \n You're a fierce, ambitious person with a kind heart. You never give up on your friends, and you always stick by the ones you love."
+    celebrity: "Beyonce",
+    celebrityDescription: "You're a fierce, ambitious person with a kind heart. You never give up on your friends, and you always stick by the ones you love.",
+    image: "https://media.giphy.com/media/niKJy6zNnyNpK/giphy.gif"
   },
 
   {
     //Kanye West 
-    celebrity: "Kanye West \n Extremely intelligent and very smart, there's a whole lot more to you then some people realize. Being different doesn't scare you. You love to be the center of attention."
+    celebrity: "Kanye West",
+    celebrityDescription: "You like to be the center of attention, but that's only because you see the world in a unique way and want to share it with others.",
+    image: "https://media.giphy.com/media/9qvkhbHg3NOKI/giphy.gif"
   }
 ]
 
 const descriptionsObject = {
-  cardiB: "Cardi B \n You're a powerful, confident badass who turns heads any time you walk in a room. You have a natural charm that people gravitate towards — once you've hooked them in, they don't want to escape!",
-  beyonce: "Beyonce \n You're a fierce, ambitious person with a kind heart. You never give up on your friends, and you always stick by the ones you love.",
-  kanye: "Kanye West \n Extremely intelligent and very smart, there's a whole lot more to you then some people realize. Being different doesn't scare you. You love to be the center of attention.",
+  // cardiB: "Cardi B \n You're a powerful, confident badass who turns heads any time you walk in a room. You have a natural charm that people gravitate towards — once you've hooked them in, they don't want to escape!",
+  // beyonce: "Beyonce \n You're a fierce, ambitious person with a kind heart. You never give up on your friends, and you always stick by the ones you love.",
+  // kanye: "Kanye West \n Extremely intelligent and very smart, there's a whole lot more to you then some people realize. Being different doesn't scare you. You love to be the center of attention.",
 }
 
 //wait for the document to be ready
@@ -120,7 +125,7 @@ $(document).ready(function () {
       }
     }
 
-    console.log(cardiB, beyonce, kanye);
+    // console.log(cardiB, beyonce, kanye);
 
     //use frequent string from the array, search the people array to find matching celebrity
 
@@ -130,20 +135,22 @@ $(document).ready(function () {
       // console.log(descriptionsObject.cardiB);
       // print title of celebrity image onto the page
       $('.sectionThreeContainer').html(`<h2>${descriptions[0].celebrity}</h2>
+                                        <p>${descriptions[0].celebrityDescription}</p>
                                         <img src=${descriptions[0].image} />`);
     }
     else if (beyonce > cardiB && beyonce > kanye) {
       // descriptions = 1;
       // console.log(descriptionsObject.beyonce);
-      $('.sectionThreeContainer').html(`<h2>${descriptions[1].celebrity}</h2>`);
+      $('.sectionThreeContainer').html(`<h2>${descriptions[1].celebrity}</h2>
+                                        <p>${descriptions[1].celebrityDescription}</p>
+                                        <img src=${descriptions[1].image} />`);
     }
     else {
       console.log(descriptionsObject.default);
-      $('.sectionThreeContainer').html(`<h2>${descriptions[2].celebrity}</h2>
-                                          `);
+      $('.sectionThreeContainer').html(`<h2> ${descriptions[2].celebrity}</h2>
+                                        <p>${descriptions[2].celebrityDescription}</p>
+                                        <img src=${descriptions[2].image} />`);
     }
-
-
 
   });
 });
